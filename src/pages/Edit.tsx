44ff0705aa02b6ae1c4934969082ac4e1abed032
안호진 <asdv94@naver.com>
2022-07-03 17:41:13 +0900

    useNavigate 사용

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,6 +1,7 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 const Edit = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const id = searchParams.get("id");
@@ -12,7 +13,24 @@ const Edit = () => {
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지</p>
-      <button onClick={() => setSearchParams({ who: "hojin" })}>QS 바꾸기</button>
+      <button onClick={() => setSearchParams({ who: "hojin" })}>
+        QS 바꾸기
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
