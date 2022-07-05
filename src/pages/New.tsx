2feb6cff6e73391f51af6612ea64eb44c9ecfb64
안호진 <asdv94@naver.com>
2022-07-06 01:48:50 +0900

    DiaryEditor 컴포넌트 화

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -1,9 +1,10 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 const New = () => {
   return (
-    <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성 페이지</p>
-    </div>
+    <>
+      <DiaryEditor />
+    </>
   );
 };
 
